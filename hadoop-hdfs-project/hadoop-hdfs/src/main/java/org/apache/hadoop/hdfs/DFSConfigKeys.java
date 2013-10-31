f4d2600c3fce80b431b427406f47f04361882041
Salman Niazi <smkniazi@kth.se>
2013-10-31 11:40:52 +0100

    changed leader election timer to 1 sec changed the retries for "ReplicationNotReached" exception to 10

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSConfigKeys.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSConfigKeys.java
@@ -59,7 +59,7 @@ public class DFSConfigKeys extends CommonConfigurationKeys {
   public static final String  DFS_NAMENODES_RPC_ADDRESS_KEY = "dfs.namenodes.rpc.address";  // format {ip:port, ip:port, ip:port} comma separated
   public static final String  DFS_NAMENODE_SELECTOR_POLICY_KEY = "dfs.namenode.selector-policy";
   public static final String  DFS_LEADER_CHECK_INTERVAL_KEY = "dfs.leader.check.interval";
-  public static final int     DFS_LEADER_CHECK_INTERVAL_DEFAULT = 3*1000; // 1 second 
+  public static final int     DFS_LEADER_CHECK_INTERVAL_DEFAULT = 1*1000; // 1 second 
   public static final String  DFS_LEADER_MISSED_HB_THRESHOLD = "dfs.leader.missed.hb";
   public static final int     DFS_LEADER_MISSED_HB_THRESHOLD_DEFAULT = 1;
   
@@ -70,7 +70,7 @@ public class DFSConfigKeys extends CommonConfigurationKeys {
   public static final int     DFS_NAME_SPACE_ID_DEFAULT = 911; // :) 
   
   public static final String  DFS_CLIENT_RETRIES_ON_FAILURE = "dfs.clinet.max.retires.on.failure";
-  public static final int     DFS_CLIENT_RETRIES_ON_FAILURE_DEFAULT = 1; //min value is 0. 
+  public static final int     DFS_CLIENT_RETRIES_ON_FAILURE_DEFAULT = 5; //min value is 0. 
   
   public static final String  DFS_CLIENT_REFRESH_NAMENODE_LIST = "dsf.client.refresh.namenode.list";
   public static final int     DFS_CLIENT_REFRESH_NAMENODE_LIST_DEFAULT = 60*1000; //time in milliseconds. 
