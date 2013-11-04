42c89c4d16dc0baaee811ae26cb9c2ed0cbe843f
Salman Niazi <smkniazi@kth.se>
2013-11-04 16:41:10 +0100

    instead of reading individual tar.gz files for common, hdfs, yarn etc project, we read the hadoop-2.0.4-alpha file that contains everyting.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 HOPS=/tmp/hops/hadoop
-DEPLOY=/var/www/hops/hops.tgz
+DEPLOY_FILE=hops.tgz
+DEPLOY=/var/www/hops/$DEPLOY_FILE
 
 rm $DEPLOY
 rm -rf $HOPS
 mkdir -p $HOPS
 
-COMMON=hadoop-common-2.0.4-alpha 
-HDFS=hadoop-hdfs-2.0.4-alpha
-CLIENT=hadoop-client-2.0.4-alpha
-YARN=hadoop-yarn-project-2.0.4-alpha
-MR=hadoop-mapreduce-2.0.4-alpha
+#mvn clean -f ./../pom.xml  package -Pdist -DskipTests
 
-tar xf ../hadoop-common-project/hadoop-common/target/$COMMON.tar.gz -C $HOPS
-tar xf ../hadoop-hdfs-project/hadoop-hdfs/target/$HDFS.tar.gz -C $HOPS
-tar xf ../hadoop-client/target/$CLIENT.tar.gz -C $HOPS
-tar xf ../hadoop-mapreduce-project/target/$MR.tar.gz -C $HOPS
-tar xf ../hadoop-yarn-project/target/$YARN.tar.gz -C $HOPS
+HADOOP=hadoop-2.0.4-alpha
 
-cp -rf $HOPS/$COMMON/* $HOPS/
-cp -rf $HOPS/$HDFS/* $HOPS/
-cp -rf $HOPS/$CLIENT/* $HOPS/
-cp -rf $HOPS/$YARN/* $HOPS/
-cp -rf $HOPS/$MR/* $HOPS/
-rm -rf $HOPS/$COMMON
-rm -rf $HOPS/$HDFS
-rm -rf $HOPS/$CLIENT
-rm -rf $HOPS/$YARN
-rm -rf $HOPS/$MR
+tar xf ../hadoop-dist/target/$HADOOP.tar.gz -C $HOPS
+cp -rf $HOPS/$HADOOP/* $HOPS/
+
+rm -rf $HOPS/$HADOOP
 
 cd $HOPS
 cd ..
-tar zcf $DEPLOY .
+
+tar zcf $DEPLOY_FILE .
+mv $DEPLOY_FILE $DEPLOY
