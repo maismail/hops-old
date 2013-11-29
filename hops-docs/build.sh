2062634e0061520653351aeaef9f99d4793009b6
Jim Dowling <jdowling@kth.se>
2013-11-29 19:51:32 +0100

    updates of dashboard, launching clusters

--- hops-docs/build.sh
+++ hops-docs/build.sh
@@ -2,6 +2,7 @@
 set -e
 mvn clean docbkx:generate-pdf
 
-pdftk target/docbkx/pdf/userguide.pdf cat 3-end output target/docbkx/pdf/userguide-sm.pdf
-pdftk D2_1_coversheet.pdf target/docbkx/pdf/userguide-sm.pdf cat output d21-bbc-kth.pdf
+#pdftk target/docbkx/pdf/userguide.pdf cat 3-end output target/docbkx/pdf/userguide-sm.pdf
+#pdftk D2_1_coversheet.pdf target/docbkx/pdf/userguide-sm.pdf cat output d21-bbc-kth.pdf
+pdftk D2_1_coversheet.pdf target/docbkx/pdf/userguide.pdf cat output d21-bbc-kth.pdf
 evince d21-bbc-kth.pdf&
