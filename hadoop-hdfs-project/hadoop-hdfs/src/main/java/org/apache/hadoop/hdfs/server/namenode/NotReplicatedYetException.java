c121ea7e15d3a1f89d66d028a653f9c538252159
Steffen Grohsschmiedt <steffeng@kth.se>
2014-06-01 16:51:31 +0200

    Undo retry hack

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NotReplicatedYetException.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NotReplicatedYetException.java
@@ -18,18 +18,17 @@
 
 package org.apache.hadoop.hdfs.server.namenode;
 
-import java.io.IOException;
-
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
-import se.sics.hop.exception.StorageException;
+
+import java.io.IOException;
 
 /**
  * The file has not finished being written to enough datanodes yet.
  */
 @InterfaceAudience.Private
 @InterfaceStability.Evolving
-public class NotReplicatedYetException extends StorageException {
+public class NotReplicatedYetException extends IOException {
   private static final long serialVersionUID = 1L;
 
   public NotReplicatedYetException(String msg) {
