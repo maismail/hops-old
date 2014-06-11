8930d605e098c74b260527007444f5e5e4c44365
Salman Niazi <smkniazi@kth.se>
2014-06-11 15:09:51 +0200

    fixed the rename script

--- scripts/deployment_scripts/hop_conf/scripts/rename.sh
+++ scripts/deployment_scripts/hop_conf/scripts/rename.sh
@@ -7,16 +7,16 @@ source $config_params
 
 hdfs_site_xml=$HOP_Dist_Folder/hop_conf/hdfs_configs/hdfs-site.xml
 
-perl -pi -e 's/'NN_MACHINE_NAME'/'$namenode'/g' $hdfs_site_xml
-perl -pi -e 's/'Dfs_Namenode_Logging_Level_Config_Param'/'$Dfs_Namenode_Logging_Level_Config_Param'/g' $hdfs_site_xml
-perl -pi -e 's/'Dfs_Namenode_Http_Address_Config_Param'/'$Dfs_Namenode_Http_Address_Config_Param'/g' $hdfs_site_xml
-perl -pi -e 's/'Dfs_Namenode_Rpc_Address_Config_Param'/'$Dfs_Namenode_Rpc_Address_Config_Param'/g' $hdfs_site_xml
-perl -pi -e 's/'Dfs_Namenode_Servicerpc_Address_Config_Param'/'$Dfs_Namenode_Servicerpc_Address_Config_Param'/g' $hdfs_site_xml
-perl -pi -e 's/'Dfs_Datanode_Address_Config_Param'/'$Dfs_Datanode_Address_Config_Param'/g' $hdfs_site_xml
-perl -pi -e 's/'Dfs_Datanode_Http_Address_Config_Param'/'$Dfs_Datanode_Http_Address_Config_Param'/g' $hdfs_site_xml
-perl -pi -e 's/'Dfs_Datanode_Ipc_Address_Config_Param'/'$Dfs_Datanode_Ipc_Address_Config_Param'/g' $hdfs_site_xml
-perl -pi -e 's/'Dfs_BlockSize_Config_Param'/'$Dfs_BlockSize_Config_Param'/g' $hdfs_site_xml
-perl -pi -e 's/'Dfs_Datanode_Data_Dir_Config_Param'/'$Dfs_Datanode_Data_Dir_Config_Param'/g' $hdfs_site_xml
+sed -i 's|NN_MACHINE_NAME|'$namenode'|g' $hdfs_site_xml
+sed -i 's|Dfs_Namenode_Logging_Level_Config_Param|'$Dfs_Namenode_Logging_Level_Config_Param'|g' $hdfs_site_xml
+sed -i 's|Dfs_Namenode_Http_Address_Config_Param|'$Dfs_Namenode_Http_Address_Config_Param'|g' $hdfs_site_xml
+sed -i 's|Dfs_Namenode_Rpc_Address_Config_Param|'$Dfs_Namenode_Rpc_Address_Config_Param'|g' $hdfs_site_xml
+sed -i 's|Dfs_Namenode_Servicerpc_Address_Config_Param|'$Dfs_Namenode_Servicerpc_Address_Config_Param'|g' $hdfs_site_xml
+sed -i 's|Dfs_Datanode_Address_Config_Param|'$Dfs_Datanode_Address_Config_Param'|g' $hdfs_site_xml
+sed -i 's|Dfs_Datanode_Http_Address_Config_Param|'$Dfs_Datanode_Http_Address_Config_Param'|g' $hdfs_site_xml
+sed -i 's|Dfs_Datanode_Ipc_Address_Config_Param|'$Dfs_Datanode_Ipc_Address_Config_Param'|g' $hdfs_site_xml
+sed -i 's|Dfs_BlockSize_Config_Param|'$Dfs_BlockSize_Config_Param'|g' $hdfs_site_xml
+sed -i 's|Dfs_Datanode_Data_Dir_Config_Param|'$Dfs_Datanode_Data_Dir_Config_Param'|g' $hdfs_site_xml
 
 
 
