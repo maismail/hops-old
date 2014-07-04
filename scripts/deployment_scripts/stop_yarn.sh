2793938c34cc9d4c0ec06750eae0cb8e67756c59
Salman Niazi <smkniazi@kth.se>
2014-07-04 17:30:33 +0200

    fixing scripts

---
+++ scripts/deployment_scripts/stop_yarn.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Author: Salman Niazi 2014
+# This script broadcasts all files required for running a HOP instance.
+# A password-less sign-on should be setup prior to calling this script
+
+
+#load config parameters
+source deployment.properties
+
+connectStr="$HOP_User@${YARN_MASTER[0]}"
+echo "Stopping ResourceManager, ProxyServer and HistoryServer on ${YARN_MASTER[0]}"
+ssh $connectStr  $HOP_Dist_Folder/sbin/yarn-daemon.sh stop resourcemanager &
+ssh $connectStr  $HOP_Dist_Folder/sbin/yarn-daemon.sh stop proxyserver &
+ssh $connectStr  $HOP_Dist_Folder/sbin/mr-jobhistory-daemon.sh stop historyserver &
+
+
+for i in ${HOP_DN_List[@]}
+do
+	echo "Stopping NodeManager on $i"
+	connectStr="$HOP_User@$i"
+	ssh $connectStr  $HOP_Dist_Folder/sbin/yarn-daemon.sh stop nodemanager &
+done
